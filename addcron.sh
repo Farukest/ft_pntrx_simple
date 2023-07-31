ed9cd5d58f0d6c3d9e6b6cc107d9cdc9b7bf94b6
farukest <abdullahfarukozden@gmail.com>
2023-07-31 17:37:12 +0300

    cron fix

--- addcron.sh
+++ addcron.sh
@@ -1,5 +1,4 @@
-(crontab -l; echo "  * *   *   *   *    /bin/bash /home/"$1"/checkmiddles.sh "$1"
- *  */50  *  *  *    /bin/bash /home/"$1"/activity_fix.sh "$2"
+(crontab -l; echo "  
  0 3  */1  *   *    /bin/bash /home/"$1"/clearlogs.sh
  
  "; ) | crontab -
